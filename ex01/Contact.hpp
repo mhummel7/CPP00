1d7a3934bddb30efea5c5d1bf12bace4baebeee0
Manuel <mhummel@student.42heilbronn.de>
2024-11-17 16:28:10 +0100

    Create Contact.hpp
    Creation of contact header, included the class contact and made some private and public

---
+++ ex01/Contact.hpp
@@ -0,0 +1,31 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <string>
+
+class Contact {
+private:
+	std::string firstName;
+	std::string lastName;
+	std::string nickname;
+	std::string phoneNumber;
+	std::string darkestSecret;
+
+public:
+	Contact();
+	~Contact();
+
+	void setFirstName(std::string name);
+	void setLastName(std::string name);
+	void setNickname(std::string nuck);
+	void setPhoneNumber(std::string number);
+	void setDarkestSecret(std::string secret);
+
+	std::string getFirstName() const;
+	std::string getLastName() const;
+	std::string getNickname() const;
+	std::string getPhoneNumber() const;
+	std::string getDarkestSecret() const;
+};
+
+#endif
