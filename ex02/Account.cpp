01976ca9b5727be606de4142bb89a57696545ba8
Manuel <mhummel@student.42heilbronn.de>
2024-11-21 11:04:27 +0100

    Added static variables
    Initializing static variables

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 10:34:16 by mhummel           #+#    #+#             */
-/*   Updated: 2024/11/21 11:03:34 by mhummel          ###   ########.fr       */
+/*   Updated: 2024/11/21 11:04:07 by mhummel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,10 @@
 #include <ctime>
 #include <iomanip>
 #include <iostream>
+
+// Initialize static variables
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
